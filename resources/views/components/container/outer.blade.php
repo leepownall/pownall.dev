67bf54b62293ff496586a6d71e22fe564522ceb3
Lee Pownall <lee@pownall.uk>
2023-09-15 19:28:25 +0100

    updates

--- resources/views/components/container/outer.blade.php
+++ resources/views/components/container/outer.blade.php
@@ -1,3 +1,3 @@
 <div {{ $attributes->merge(['class' => 'sm:px-8']) }}>
-    <div class="mx-auto max-w-7xl lg:px-8">{{ $slot }}</div>
+    <div class="max-w-7xl lg:px-8">{{ $slot }}</div>
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,39 @@
 <x-layouts.app>
-    <x-container class="mt-6 sm:mt-9">
-        <div class="max-w-4xl">
-            <h1 class="text-4xl sm:text-5xl font-extrabold inline-block text-slate-800">
-                Lee Pownall
-            </h1>
-            <div class="mt-6 sm:mt-10">
-                A PHP Developer from the West Midlands. I specialise in Laravel and focus on writing clean, maintainable, and testable code.
-            </div>
-            <div class="mt-6 sm:mt-10 flex justify-center sm:justify-start">
-                <ul class="flex flex-row flex-wrap space-x-2">
-                    <li><a class="text-zinc-600 hover:underline" href="https://twitter.com/leepownall" target="_blank">Twitter</a></li>
-                    <li>•</li>
-                    <li><a class="text-zinc-600 hover:underline" href="https://github.com/leepownall" target="_blank">GitHub</a></li>
-                    <li>•</li>
-                    <li><a class="text-zinc-600 hover:underline" href="https://www.linkedin.com/in/lee-pownall" target="_blank">LinkedIn</a></li>
-                    <li>•</li>
-                    <li><a class="text-zinc-600 hover:underline" href="mailto:lee@pownall.uk">Email</a></li>
-                </ul>
-            </div>
+    <div class="flex flex-col space-y-6 sm:space-y-10 mt-6 sm:mt-9 px-2 sm:px-8 max-w-3xl">
+        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Lee Pownall
+        </h1>
+        <div>
+            A PHP Developer from the West Midlands. I specialise in Laravel and focus on writing clean, maintainable, and testable code.
         </div>
-    </x-container>
+        <div>
+            <h2 class="text-xl font-bold tracking-tight text-gray-700 sm:text-2xl">Stuff I've made</h2>
+            <ul class="list-disc list-inside mt-4">
+                <li>
+                    <a href="https://fridaymix.xyz" class="text-blue-500 hover:underline">fridaymix</a> - App to find duplicate songs and display at what point we are in the playlist for works weekly collaborative
+                    playlist. Built on Laravel, Livewire, Alpine.js and Tailwind CSS. Source code available on <a href="https://github.com/leepownall/fridaymix" class="text-blue-500 hover:underline">GitHub</a>.
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h2 class="text-xl font-bold tracking-tight text-gray-700 sm:text-2xl">Things I've written</h2>
+            <ul class="list-disc list-inside mt-4">
+                <li>
+                    <a href="https://jump24.co.uk/journal/laravel-blade-components" class="text-blue-500 hover:underline">Laravel Blade Components</a> - Short article for
+                    <a href="https://jump24.co.uk" class="text-blue-500 hover:underline">Jump24</a> given a brief introduction to Laravel's Blade Components.
+                </li>
+            </ul>
+        </div>
+        <div class="flex justify-center sm:justify-start">
+            <ul class="flex flex-row flex-wrap space-x-2">
+                <li><a class="text-blue-500 hover:underline" href="https://twitter.com/leepownall" target="_blank">Twitter</a></li>
+                <li>•</li>
+                <li><a class="text-blue-500 hover:underline" href="https://github.com/leepownall" target="_blank">GitHub</a></li>
+                <li>•</li>
+                <li><a class="text-blue-500 hover:underline" href="https://www.linkedin.com/in/lee-pownall" target="_blank">LinkedIn</a></li>
+                <li>•</li>
+                <li><a class="text-blue-500 hover:underline" href="mailto:lee@pownall.uk">Email</a></li>
+            </ul>
+        </div>
+    </div>
 </x-layouts.app>
