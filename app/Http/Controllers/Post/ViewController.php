aec2d422079883200d6efbee3c442b77413d1448
Lee Pownall <lee@pownall.uk>
2023-09-24 13:48:52 +0100

    remove

--- app/Http/Controllers/Post/ViewController.php
+++ app/Http/Controllers/Post/ViewController.php
@@ -5,15 +5,9 @@ namespace App\Http\Controllers\Post;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Contracts\View\View;
-use League\CommonMark\ConverterInterface;
 
 class ViewController extends Controller
 {
-    public function __construct(private ConverterInterface $converter)
-    {
-
-    }
-
     public function __invoke(Post $post): View
     {
         return view('post.show', [
