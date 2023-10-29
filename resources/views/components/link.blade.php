3c6bcf512391401094fa2684d6b8313895e074f0
Lee Pownall <lee@pownall.uk>
2023-10-29 17:29:22 +0000

    wip

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -4,5 +4,5 @@
 ])
 
 <span>
-    <a href="{{ $href }}" class="text-blue-700 no-underline hover:underline"    @if($external) target="_blank" @endif>{{ $slot }}</a>@if($external)<x-icon.external class="w-4 h-4 inline text-blue-700 pl-0.5" />@endif
+    <a href="{{ $href }}" class="text-blue-700 no-underline hover:underline" @if($external) target="_blank" @endif>{{ $slot }}</a>@if($external)@endif
 </span>
