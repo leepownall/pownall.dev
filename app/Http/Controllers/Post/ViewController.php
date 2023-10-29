a47d18f5ef15cb95550defd7d3110d218a42d927
Lee Pownall <lee@pownall.uk>
2023-10-29 17:37:11 +0000

    wip

--- app/Http/Controllers/Post/ViewController.php
+++ app/Http/Controllers/Post/ViewController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Post;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Str;
 use Leshkens\LaravelReadTime\Facades\ReadTime;
 
 class ViewController extends Controller
@@ -12,7 +13,8 @@ class ViewController extends Controller
     public function __invoke(Post $post): View
     {
         $readTime = ReadTime::parse($post->content);
-        $readTime = "{$readTime->number} {$readTime->unit} read time";
+        $unit = Str::plural($readTime->unit);
+        $readTime = "{$readTime->number} {$unit}";
 
         return view('post.show', [
             'post' => $post,
